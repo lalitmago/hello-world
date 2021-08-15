253c4890f5fe4915779bc838ea1d6a1dcd8d34d7
simransingh <simran.singha30@gmail.com>
2021-08-15 03:56:51 +0100

    1st commit by Simran - 15/08/21

--- src/main/java/org/verctrl/practice/Main.java
+++ src/main/java/org/verctrl/practice/Main.java
@@ -3,5 +3,11 @@ package org.verctrl.practice;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Wjkk Wjkf...!");
+
+    /*
+     * Simran changes below:-
+     * */
+
+        System.out.println("SSA from Simran.");
     }
 }
